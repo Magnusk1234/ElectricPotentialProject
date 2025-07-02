ee2deaabac6b19a122be1633d05a96fa0b3d80a5
Magnus <magnukar@stud.ntnu.no>
2025-07-02 16:06:50 +0200

    Added potentials.py containing different functions as boundrary conditions

---
+++ potentials.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# constant potential function
+def V0_constant(Vc,x):
+    return Vc
+
+# sinus 2 potential function
+def V0_sinus_2(Vc,x):
+    return Vc*np.sin(2*np.pi*x)
+
+# sinus 3 potential function
+def V0_sinus_3(Vc,x):
+    return Vc*np.sin(3*np.pi*x)
+
+#heaviside potential function
+def V0_heaviside(Vc,x):
+    return Vc*(np.heaviside(x-0.4, 1)-np.heaviside(x-0.6, 1))
+
+# gaussian potential function
+def V0_gaussian(Vc,x):
+    return Vc*np.exp(-((x-0.5)**2)/(2*(0.1**2)))
+
+# 4th order polynomial potential function
+def V0_polynomial_4th(Vc,x):
+    return Vc*(1-(x-0.5)**4)
+
+
