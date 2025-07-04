Tried adding a function for calculating the standard deviation and correcting the function for the electric field

solver.py
@@ -7,13 +7,13 @@ def Cn(n, V0, Vc):
     Cn, _ = quad(integrand, 0, 1)
     return Cn / np.sinh(n*np.pi)
 
-def Electric_potential(x, y, N, V0, Vc):
-    potential = np.zeros((len(x),len(y)))
+def Electric_potential(X, Y, N, V0, Vc):
+    potential = np.zeros((len(X),len(Y)))
 
     for i in range(1, N+1):
         Cn_value = Cn(i, V0, Vc)
-        Vn = Cn_value * np.sinh(i * np.pi * y) * np.sin(i * np.pi * x)
-        potential += Vn
+        Vn = Cn_value * np.sinh(i * np.pi * Y) * np.sin(i * np.pi * X)
+        potential += Vn 
     return potential
 
 def Electric_field(x, y, N, potential):