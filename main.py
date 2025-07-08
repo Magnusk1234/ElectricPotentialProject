8f235348494721b15a328734ff878dd8cc10e444
Magnus <magnukar@stud.ntnu.no>
2025-07-08 13:57:38 +0200

    Tried fixing convergense test

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def main():
     Vc = 1.0
     N = 100
     resolution = 10
-    test_potential(V0_sinus_2, Vc, N, resolution)
+    N_min = 50
+    N_max = 100
+    N_interval = 10
+    #test_potential(V0_sinus_2, Vc, N, resolution)
 
     test_field(V0_sinus_2, Vc, N, resolution)
+    calculate_std(V0_sinus_2,Vc, N_min, N_max, N_interval, resolution)
 
 
 
